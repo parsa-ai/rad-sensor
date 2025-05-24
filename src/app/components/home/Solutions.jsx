5879a08b535afb18a0550dffd9102ccca5543e72
parsa <parsaalavi9@gmail.com>
2025-05-24 11:35:06 +0330

    feat: add Solutions section , add Titles component

---
+++ src/app/components/home/Solutions.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../Container'
+import Titles from '../Titles'
+
+function Solutions() {
+    return (
+        <section className='w-full h-full mt-20'>
+            <Container>
+                <Titles text='What are our solutions?'></Titles>
+            </Container>
+        </section>
+    )
+}
+
+export default Solutions
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 
 import Hero from "./components/home/Hero";
+import Solutions from "./components/home/Solutions";
 
 export default function Home() {
   return (
     <main>
       <Hero></Hero>
+      <Solutions></Solutions>
     </main>
   );
 }
