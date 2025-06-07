408db9e9f166c46a06d2cfb1840f45d793eb351b
parsa <parsaalavi9@gmail.com>
2025-06-07 11:21:54 +0330

    feat: create timeLine of About page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -44,6 +44,48 @@ function page() {
           <article className="flex-6/12">
             <img src="/images/About/hero.png" className="mix-blend-lighten" alt="" /></article>
         </section>
+        <section className="flex flex-col gap-10">
+          <div className="w-full h-1 rounded-full bg-[linear-gradient(90deg,#173E4F_0%,#33A2E7_47.12%,#173E4F_100%)]" />
+          <article className="flex justify-between">
+            <div className="flex flex-col gap-5 w-[272px] relative">
+              <img src="/images/About/time1.png" className="w-[272px] h-[182px] object-cover rounded-lg" alt="" />
+              <div className="w-5 h-5 absolute -top-13 rounded-full bg-[#42C2FA] shadow-[0px_0px_8px_-1px_#6BD2FF]" />
+              <div className="w-14 h-7 absolute -top-24 -left-4 rounded-md bg-[#99999917] shadow-[0px_2px_4.8px_0px_#FFFFFF2B_inset] flex justify-center items-center">
+                2021
+              </div>
+              <div className="flex flex-col gap-1 ">
+                <span className="text-lg font-semibold">First in Iran</span>
+                <p className="text-base font-medium text-[#BDBDBD]">Rad Sensor built Iran’s firstEC sensor, all local, affordable, and top quality for you.</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 w-[272px] relative">
+              <img src="/images/About/time2.png" className="w-[272px] h-[182px] object-cover rounded-lg" alt="" />
+              <div className="w-5 h-5 absolute -top-13 rounded-full bg-[#42C2FA] shadow-[0px_0px_8px_-1px_#6BD2FF]" />
+              <div className="w-14 h-7 absolute -top-24 -left-4 rounded-md bg-[#99999917] shadow-[0px_2px_4.8px_0px_#FFFFFF2B_inset] flex justify-center items-center">
+                2023
+              </div>
+              <div className="flex flex-col gap-1 ">
+                <span className="text-lg font-semibold">Ready to Partner</span>
+                <p className="text-base font-medium text-[#BDBDBD]">Team up with Rad Sensor
+                  a South Asia leader, participated
+                  at Dubai GITEX Exhibition.</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 w-[272px] relative">
+              <img src="/images/About/time3.png" className="w-[272px] h-[182px] object-cover rounded-lg" alt="" />
+              <div className="w-5 h-5 absolute -top-13 rounded-full bg-[#42C2FA] shadow-[0px_0px_8px_-1px_#6BD2FF]" />
+              <div className="w-14 h-7 absolute -top-24 -left-4 rounded-md bg-[#99999917] shadow-[0px_2px_4.8px_0px_#FFFFFF2B_inset] flex justify-center items-center">
+                2025
+              </div>
+              <div className="flex flex-col gap-1 ">
+                <span className="text-lg font-semibold">Growing Strong</span>
+                <p className="text-base font-medium text-[#BDBDBD]">Invest in Rad Sensor, a top 
+high-tech name since 2019,
+ready for big success ahead.</p>
+              </div>
+            </div>
+          </article>
+        </section>
         <section className="flex flex-col gap-14">
           <h3 className="text-4xl font-medium">
             Core Values
@@ -131,7 +173,7 @@ function page() {
             <p>Powering ideas — from bold concepts to real impact</p>
           </div>
           <Link href={'/Contact'} className='w-[267px] h-[57px] flex justify-center items-center text-[#3B3B3B] bg-[#f7f7f7] rounded-lg gap-1.5 font-medium text-lg ' >
-           Get in Touch
+            Get in Touch
             <ArrowIcon className={'fill-black w-5 rotate-45'}></ArrowIcon>
           </Link>
         </section>
