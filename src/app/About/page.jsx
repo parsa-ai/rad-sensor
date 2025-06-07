37a67eecf594fabe9715850ced96e8a0d175d793
parsa <parsaalavi9@gmail.com>
2025-06-07 10:40:18 +0330

    feat: create Core Values and Meet the Team for About page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -2,9 +2,9 @@ import Container from "../components/ui/Container"
 
 function page() {
   return (
-    <main className="w-full">
-      <Container>
-        <section className="w-full flex">
+    <main className="w-full pb-32">
+      <Container className='flex flex-col gap-32'>
+        <section className="w-full flex ">
           <article className="flex-6/12 pt-32 flex flex-col justify-between h-[550px]" >
             <header>
               <h2 className="text-6xl/normal font-medium ">
@@ -17,7 +17,7 @@ function page() {
             <ul className="flex justify-between">
               <li>
                 <div className="relative h-14 flex items-center">
-                  <div className="w-[5px] rounded-full top-0 absolute bg-[#16B2FF] h-full"/>
+                  <div className="w-[5px] rounded-full top-0 absolute bg-[#16B2FF] h-full" />
                   <span className="pl-3 text-5xl font-semibold">1st</span>
                 </div>
                 <p className="pl-3">EC Innovator<br></br>
@@ -25,14 +25,14 @@ function page() {
               </li>
               <li>
                 <div className="relative h-14 flex items-center">
-                  <div className="w-[5px] rounded-full top-0 absolute bg-[#16B2FF] h-full"/>
+                  <div className="w-[5px] rounded-full top-0 absolute bg-[#16B2FF] h-full" />
                   <span className="pl-3 text-5xl font-semibold">+5</span>
                 </div>
                 <p className="pl-3">Years of Experience</p>
               </li>
               <li>
                 <div className="relative h-14 flex items-center">
-                  <div className="w-[5px] rounded-full top-0 absolute bg-[#16B2FF] h-full"/>
+                  <div className="w-[5px] rounded-full top-0 absolute bg-[#16B2FF] h-full" />
                   <span className="pl-3 text-5xl font-semibold">+20</span>
                 </div>
                 <p className="pl-3">B2B Partners</p>
@@ -40,9 +40,58 @@ function page() {
             </ul>
           </article>
           <article className="flex-6/12">
-          <img src="/images/About/hero.png" className="mix-blend-lighten" alt="" /></article>
+            <img src="/images/About/hero.png" className="mix-blend-lighten" alt="" /></article>
         </section>
-        
+        <section className="flex flex-col gap-14">
+          <h3 className="text-4xl font-medium">
+            Core Values
+          </h3>
+          <article >
+            <Container className="flex justify-between items-center">
+              <img src="/images/About/Core1.png" className="h-fit w-fit" alt="" />
+              <img src="/images/About/Core2.png" className="h-fit w-fit" alt="" />
+              <img src="/images/About/Core3.png" className="h-fit w-fit" alt="" />
+              <img src="/images/About/Core4.png" className="h-fit w-fit" alt="" />
+            </Container>
+          </article>
+        </section>
+        <section className="flex flex-col gap-14">
+          <h3 className="text-4xl font-medium">
+            Meet the Team
+          </h3>
+          <article className="flex justify-between">
+            <div className="w-72 pb-4  bg-[#29282F] p-2 shadow-[0px_4px_50.6px_0px_#8686864D_inset] rounded-2xl flex flex-col gap-2.5">
+              <img src="/images/About/Meet.png" className="w-full rounded-t-xl" alt="" />
+              <div className="px-2 flex flex-col">
+                <span className="text-xl font-semibold">Babak Mokhtari</span>
+                <span className="text-base font-medium">CEO & Co - Founder</span>
+              </div>
+            </div>
+
+            <div className="w-72 pb-4  bg-[#29282F] p-2 shadow-[0px_4px_50.6px_0px_#8686864D_inset] rounded-2xl flex flex-col gap-2.5">
+              <img src="/images/About/Meet.png" className="w-full rounded-t-xl" alt="" />
+              <div className="px-2 flex flex-col">
+                <span className="text-xl font-semibold">Babak Mokhtari</span>
+                <span className="text-base font-medium">CEO & Co - Founder</span>
+              </div>
+            </div>
+            <div className="w-72 pb-4  bg-[#29282F] p-2 shadow-[0px_4px_50.6px_0px_#8686864D_inset] rounded-2xl flex flex-col gap-2.5">
+              <img src="/images/About/Meet.png" className="w-full rounded-t-xl" alt="" />
+              <div className="px-2 flex flex-col">
+                <span className="text-xl font-semibold">Babak Mokhtari</span>
+                <span className="text-base font-medium">CEO & Co - Founder</span>
+              </div>
+            </div>
+            <div className="w-72 pb-4  bg-[#29282F] p-2 shadow-[0px_4px_50.6px_0px_#8686864D_inset] rounded-2xl flex flex-col gap-2.5">
+              <img src="/images/About/Meet.png" className="w-full rounded-t-xl" alt="" />
+              <div className="px-2 flex flex-col">
+                <span className="text-xl font-semibold">Babak Mokhtari</span>
+                <span className="text-base font-medium">CEO & Co - Founder</span>
+              </div>
+            </div>
+            
+          </article>
+        </section>
       </Container>
     </main>
   )
