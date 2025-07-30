dab3a3593f7f50ade48858dd1ba9b8b156492518
parsa <parsaalavi9@gmail.com>
2025-07-30 12:03:37 +0330

    feat: add Swiper component to WhyUs section and update styles for improved layout

--- src/app/page.js
+++ src/app/page.js
@@ -15,8 +15,8 @@ export default function Home() {
        <Partners></Partners>
       <Years></Years>
       <About></About>
-      {/*<WhyUs></WhyUs>
-      <Blog></Blog> */}
+      <WhyUs></WhyUs>
+      {/*<Blog></Blog> */}
     </main>
   );
 }
