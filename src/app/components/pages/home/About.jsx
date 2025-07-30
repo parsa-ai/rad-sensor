e58fd98c90da2e52186728cb8c21ca47381d5c38
parsa <parsaalavi9@gmail.com>
2025-07-30 12:14:32 +0330

    feat: improve About component layout and responsiveness with updated styles

--- src/app/components/pages/home/About.jsx
+++ src/app/components/pages/home/About.jsx
@@ -3,16 +3,16 @@ import HomeAbout from "../../ui/icons/HomeAbout"
 
 function About() {
     return (
-        <section className="relative flex items-center justify-center mb-28">
+        <section className="relative flex items-center justify-center md:mb-28">
             <Container className="flex flex-col md:flex-row justify-between gap-10 md:gap-28 h-auto md:h-[470px] relative">
-                <article className="flex-1 relative min-h-[270px] md:min-h-[470px]">
-                    <div className="w-[300px] h-[200px] md:w-[400px] md:h-[270px] absolute top-0 right-0 md:right-10 z-0 rounded-2xl">
+                <article className="flex-1 relative min-h-[270px] md:min-h-[470px] flex flex-col md:block gap-6">
+                    <div className="w-full h-[200px] md:w-[400px] md:h-[270px] md:absolute top-0 right-0 md:right-10 z-0 rounded-2xl">
                         <div className="w-16 h-16 md:w-24 md:h-24 bg-white absolute -right-5 md:-right-10 opacity-5 rounded-full -top-5 md:-top-10 -z-10"></div>
-                        <img src="/images/HomeAbout/p2.png" className="w-full h-full rounded-2xl object-cover" alt="" />
+                        <img src="/images/HomeAbout/p2.png" className="w-full h-full rounded-2xl object-cover object-top" alt="" />
                     </div>
-                    <div className="w-[200px] h-[180px] md:w-[280px] md:h-[260px] absolute bottom-0 left-0 md:left-10 z-10 rounded-2xl">
+                    <div className="w-full h-[200px] md:w-[280px] md:h-[260px] md:absolute bottom-0 left-0 md:left-10 z-10 rounded-2xl">
                         <div className="w-16 h-16 md:w-24 md:h-24 bg-white absolute -top-5 md:-top-10 opacity-5 rounded-full -left-5 md:-left-10 -z-10"></div>
-                        <img src="/images/HomeAbout/p1.jpg" className="w-full h-full rounded-2xl object-cover" alt="" />
+                        <img src="/images/HomeAbout/p1.jpg" className="w-full h-full rounded-2xl object-cover " alt="" />
                     </div>
                 </article>
                 <article className="flex-1 flex flex-col gap-6 md:gap-10 text-center md:text-left">
